d8b349b6e84a3fcc92c3be8503ab4bbd58fb6d36
Rafaela Ermacora Soares <rermacor@c3r5p3.42sp.org.br>
2026-06-04 18:32:25 -0300

    danone

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rermacor <rermacor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 15:06:15 by rermacor          #+#    #+#             */
-/*   Updated: 2026/06/03 15:23:18 by rermacor         ###   ########.fr       */
+/*   Updated: 2026/06/04 18:12:53 by rermacor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 
-	 
+	if (s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return (NULL);
+	while ()
 }
